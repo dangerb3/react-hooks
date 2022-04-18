afaa464155b4ed3762932c6ee21361a73bd10e2c
dangerb3 <dangerb@yandex.ru>
2022-04-19 01:50:28 +0300

    useInput hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import useInput from './hooks/useInput';
 
 function App() {
+  const username = useInput('');
+  const password = useInput('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input {...username} type="text" placeholder="username" />
+      <input {...password} type="text" placeholder="password" />
+      <button onClick={() => console.log(username, password)}>Click</button>
     </div>
   );
 }
